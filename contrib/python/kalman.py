436565181e152b86a58edd2d85b7a963fee13c3e
Sifan YE <sye8@u.rochester.edu>
2017-12-14 20:29:20 -0500

    Update kalman.py

--- contrib/python/kalman.py
+++ contrib/python/kalman.py
@@ -70,12 +70,3 @@ class KalmanFilter:
     def set_process_noise(self, noise):
         self.R = noise
 
-# Main method as an example
-def main():
-    test = KalmanFilter(0.008, 0.1)
-    testData = [66,64,63,63,63,66,65,67,58]
-    for x in testData:
-        print "Data:", x
-        print "Filtered Data: ", test.filter(x)
-
-main()
