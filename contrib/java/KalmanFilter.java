d181a33bfcf983f279f0b935149a1dd183fb4e2a
Sifan YE <sye8@u.rochester.edu>
2017-12-14 20:12:08 -0500

    Remove main method

--- contrib/java/KalmanFilter.java
+++ contrib/java/KalmanFilter.java
@@ -85,18 +85,4 @@ public class KalmanFilter {
 	public void setProcessNoise(double noise){
 		    this.R = noise;
 	}
-    
-    /**
-      * Main method for usage example
-      *
-      */
-    public static void main(String[] args){
-        KalmanFilter test = new KalmanFilter(0.008, 0.1);
-        
-        double[] testData = {66,64,63,63,63,66,65,67,58};
-        for(double x: testData){
-            System.out.println("Input data: " + x);
-            System.out.println("Filtered data: " + test.filter(x));
-        }
-    }
 }
