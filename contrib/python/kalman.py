d21617d30b294a8d8da058263bebfd73614b1e3e
Sifan YE <sye8@u.rochester.edu>
2017-12-15 01:12:12 -0500

    Remove main()

--- contrib/python/kalman.py
+++ contrib/python/kalman.py
@@ -67,13 +67,3 @@ class KalmanFilter:
     # noise: The new measurement noise
     def set_process_noise(self, noise):
         self.R = noise
-
-# Main method as an example
-def main():
-    test = KalmanFilter(0.008, 0.1)
-    testData = [66,64,63,63,63,66,65,67,58]
-    for x in testData:
-        print "Data:", x
-        print "Filtered Data: ", test.filter(x)
-
-main()
