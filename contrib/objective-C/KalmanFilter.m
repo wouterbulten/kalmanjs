742e10c79169df66a3f5fb74348c9ee5036b251b
Biraj Dhakal <31617102+bidh@users.noreply.github.com>
2018-11-29 17:48:42 +0100

    Update Kalmanfilter.m

--- contrib/objective-C/KalmanFilter.m
+++ contrib/objective-C/KalmanFilter.m
@@ -17,12 +17,13 @@
 
 #import <Foundation/Foundation.h>
 
-@interface SampleClass:NSObject
+@interface KalmanFilter:NSObject
 /* method declaration */
 - (double)filter:(double)measurement;
+- (double)filter:(double)measurement:(double)u;
 @end
 
-@implementation SampleClass
+@implementation KalmanFilter
 double cov=NAN;
 double x=NAN;
 - (double)filter:(double)measurement {
@@ -46,32 +47,26 @@ double x=NAN;
     }
     return x;
 }
+- (double)filter:(double)measurement:(double)u {
+    double A=1;
+    double B=0;
+    double C=1;
+    double R=0.008;
+    double Q=0.1;
+    if(isnan(x)){
+        x = (1/C) * measurement;
+        cov = (1/C) * Q * (1/C);
+    }else{
+        double predX = (A*x) + (B*u);
+        double predCov = ((A * cov) * A) + R;
+        
+        double K = predCov * C * (1/((C * predCov * C) + Q));
+        
+        x = predX + K *(measurement - (C * predX));
+        cov = predCov - (K * C * predCov);
+    }
+    return x;
+}
 
 @end
 
-int main () {
-    
-    double ret;
-    SampleClass *sampleClass = [[SampleClass alloc]init];
-    
-    NSMutableArray *pointA10Recordings= [[NSMutableArray alloc] init];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:12.59]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:8.91]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:10.0]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:12.59]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:7.94]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:11.22]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:12.59]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:10.0]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:12.59]];
-    [pointA10Recordings addObject:[NSNumber numberWithDouble:10.0]];
-    
-    
-    
-    for(int i=0;i<10;i++){
-        double value   = [[pointA10Recordings objectAtIndex:i] doubleValue];
-        ret = [sampleClass filter:value];
-        NSLog(@"Filtered value is : %.2f\n", ret );
-    }
-    return 0;
-}
